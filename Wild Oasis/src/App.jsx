5e6daa04651aebb3ac00d6828e2c8522fa4bbe4b
jeewantha upul <upuljeewantha070@gmail.com>
2024-12-06 16:28:32 +0530

    set up the basic pages and routes

---
+++ Wild Oasis/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<HomePage />} />
+          <Route path={"/product"} element={<Product />} />
+          <Route path={"/pricing"} element={<Pricing />} />
+          <Route path={"/app"} element={<AppLayout />} />
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"*"} element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
