3faae07fd55f95b4ac39ad48db2c4c88cf30fd32
jeewantha upul <upuljeewantha070@gmail.com>
2024-12-12 17:17:14 +0530

    added programmetic navigations

--- Wild Oasis/src/App.jsx
+++ Wild Oasis/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 import HomePage from "./pages/HomePage";
@@ -10,6 +10,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -42,7 +43,7 @@ function App() {
           <Route path={"product"} element={<Product />} />
           <Route path={"pricing"} element={<Pricing />} />
           <Route path={"app"} element={<AppLayout />}>
-            <Route index element={<p>List</p>} />
+            <Route index element={<Navigate replace to="cities" />} />
             <Route
               path={"cities"}
               element={<CityList cities={cities} isLoading={isLoading} />}
@@ -52,7 +53,7 @@ function App() {
               path={"countries"}
               element={<CountryList cities={cities} isLoading={isLoading} />}
             />
-            <Route path={"form"} element={<p>form</p>} />
+            <Route path={"form"} element={<Form />} />
           </Route>
           <Route path={"login"} element={<Login />} />
           <Route path={"*"} element={<PageNotFound />} />
--- Wild Oasis/src/components/Map.jsx
+++ Wild Oasis/src/components/Map.jsx
@@ -1,8 +1,9 @@
 import styles from "./Map.module.css";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
@@ -12,7 +13,7 @@ function Map() {
   };
 
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       Map {lat} and {lng}
       <button onClick={changeLat}></button>
     </div>
