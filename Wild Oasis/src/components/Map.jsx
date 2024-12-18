6b2fe3905f8a15d785dd936e7cf17c5d59cbe884
jeewantha upul <upuljeewantha070@gmail.com>
2024-12-18 13:19:00 +0530

    added city markers on map

--- Wild Oasis/src/components/Map.jsx
+++ Wild Oasis/src/components/Map.jsx
@@ -2,6 +2,7 @@
 import styles from "./Map.module.css";
 import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useCities } from "../contexts/CitiesContext";
 // import { useSearchParams } from "react-router-dom";
 
 function Map() {
@@ -14,6 +15,9 @@ function Map() {
   // const changeLat = () => {
   //   setSearchParams();
   // };
+
+  // getting the cities from global state
+  const { cities } = useCities();
 
   const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
   return (
@@ -28,11 +32,18 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
