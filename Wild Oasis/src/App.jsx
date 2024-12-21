6c4d60d1ceaee3db2a6d15de4a1689094864f39d
jeewantha <upuljeewantha070@gmail.com>
2024-12-21 20:29:31 +0530

    added Lazy loading functionality to app

--- Wild Oasis/src/App.jsx
+++ Wild Oasis/src/App.jsx
@@ -1,17 +1,26 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { FakeAuthProvider } from "./contexts/FakeAuthContext";
+import { lazy, Suspense } from "react";
+import SpinnerFullPage from "../src/components/SpinnerFullPage";
 
-import HomePage from "./pages/HomePage";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import AppLayout from "./pages/AppLayout";
-import PageNotFound from "./pages/PageNotFound";
-import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { FakeAuthProvider } from "./contexts/FakeAuthContext";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const Login = lazy(() => import("./pages/Login"));
+// import HomePage from "./pages/HomePage";
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import AppLayout from "./pages/AppLayout";
+// import PageNotFound from "./pages/PageNotFound";
+// import Login from "./pages/Login";
 import { ProtectedRoutes } from "./pages/ProtectedRoutes";
 
 function App() {
@@ -19,27 +28,29 @@ function App() {
     <CitiesProvider>
       <FakeAuthProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<HomePage />} />
-            <Route path={"product"} element={<Product />} />
-            <Route path={"pricing"} element={<Pricing />} />
-            <Route
-              path={"app"}
-              element={
-                <ProtectedRoutes>
-                  <AppLayout />
-                </ProtectedRoutes>
-              }
-            >
-              <Route index element={<Navigate replace to="cities" />} />
-              <Route path={"cities"} element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path={"countries"} element={<CountryList />} />
-              <Route path={"form"} element={<Form />} />
-            </Route>
-            <Route path={"login"} element={<Login />} />
-            <Route path={"*"} element={<PageNotFound />} />
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<HomePage />} />
+              <Route path={"product"} element={<Product />} />
+              <Route path={"pricing"} element={<Pricing />} />
+              <Route
+                path={"app"}
+                element={
+                  <ProtectedRoutes>
+                    <AppLayout />
+                  </ProtectedRoutes>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path={"cities"} element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path={"countries"} element={<CountryList />} />
+                <Route path={"form"} element={<Form />} />
+              </Route>
+              <Route path={"login"} element={<Login />} />
+              <Route path={"*"} element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </FakeAuthProvider>
     </CitiesProvider>
