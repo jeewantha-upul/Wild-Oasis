081988e1e7240d29e504225fbbd23b6d7f8b5f2d
jeewantha <upuljeewantha070@gmail.com>
2024-12-21 21:03:44 +0530

    changed the zoom level of the map container

--- Wild Oasis/src/components/Map.jsx
+++ Wild Oasis/src/components/Map.jsx
@@ -51,7 +51,7 @@ function Map() {
       )}
       <MapContainer
         center={mapPosition}
-        zoom={6}
+        zoom={10}
         scrollWheelZoom={true}
         className={styles.map}
       >
